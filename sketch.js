6d011903f4c2fb449fb2d9b8b9353bec1a6381a1
courtneyhale <courtneyhale44@gmail.com>
2016-02-11 21:45:02 -0500

    Daily 2

--- sketch.js
+++ sketch.js
@@ -1,11 +1,28 @@
 var bubbles = [];
 
+var updateAndDisplay = function (bubble) {
+
+      bubble.update();
+      bubble.display();
+};
+
+var addNumBubbles = function (totalBubbles) {
+  while (bubbles.length < totalBubbles) {
+    bubbles.push(new Bubble(random(0, width), random(0, height)));
+  }
+};
+
 setup = function () {
   createCanvas(600, 600);
   // your code goes here
+  addNumBubbles(500);
 };
 
 draw = function () {
   background(0);
-  // your code goes here
+
+
+  if(mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY));
+
+  bubbles.forEach(updateAndDisplay);
 };
